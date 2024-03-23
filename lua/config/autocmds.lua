f3d1285afa8bfe7a873c4c29b1b92f75b5ba112e
Jandrel Valerio <jifvalerio@gmail.com>
2024-03-24 01:54:57 +0800

    Update plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,10 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable comments on new line
+local function augroup(name)
+  return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+end
+
+-- :help fo-table
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
+  desc = "Stop comments from continuing on new line",
+  group = augroup("stop_comments"),
   callback = function()
-    vim.opt_local.formatoptions:remove({ "r", "o" })
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+-- This will show current line git blame
+-- Also adds a keymap for toggling it off
+
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      {
+        "<leader>uB",
+        "<cmd>Gitsigns toggle_current_line_blame<cr>",
+        desc = "Toggle Line Blame",
+      },
+    },
+    opts = {
+      current_line_blame = true,
+    },
+  },
+}
