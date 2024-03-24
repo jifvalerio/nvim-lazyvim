14c75c7dffaeee087098a694508406c65af1e585
Jandrel Valerio <jifvalerio@gmail.com>
2024-03-24 20:26:31 +0800

    Show git blame on current line

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,17 +2,15 @@
 -- Also adds a keymap for toggling it off
 
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    keys = {
-      {
-        "<leader>uB",
-        "<cmd>Gitsigns toggle_current_line_blame<cr>",
-        desc = "Toggle Line Blame",
-      },
+  "lewis6991/gitsigns.nvim",
+  keys = {
+    {
+      "<leader>uB",
+      "<cmd>Gitsigns toggle_current_line_blame<cr>",
+      desc = "Toggle Line Blame",
     },
-    opts = {
-      current_line_blame = true,
-    },
+  },
+  opts = {
+    current_line_blame = true,
   },
 }
