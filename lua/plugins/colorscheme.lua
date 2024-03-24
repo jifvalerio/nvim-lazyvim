043d10a3a28f2dba56dac7a3471d61544f575965
Jandrel Valerio <jifvalerio@gmail.com>
2024-03-24 20:20:57 +0800

    Set colorscheme to rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
+  { "rose-pine/neovim", name = "rose-pine" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "rose-pine",
     },
   },
 }
